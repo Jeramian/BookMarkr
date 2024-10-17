42fd2a68fe4b08b088949d2e4c6b65730e0a80d6
Jeramian <jeremyleramian@outlook.com>
2024-10-16 18:00:45 -0700

    small issue fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import NavBar from './components/NavBar';
 import Banner from './components/Banner';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'BookMarkr',
@@ -18,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Banner />
         <NavBar />
         {children}
